53465158459a32c8046efdad0466a72fc4fffe28
asmaasaleh185 <asmaasaleh185@gmail.com>
2024-07-06 02:56:22 +0200

    initial commit

---
+++ Form/Form.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\core\Form;
+
+use app\core\Model;
+use app\core\Form\InputField;
+
+class Form{
+    public static function begin($action, $method){
+        echo sprintf('<form action="%s" method="%s">', $action, $method);
+        return new Form();
+
+    }
+    public static function end(){
+        echo '</form>';
+    }
+    public function field(Model $model, $attribute){
+        return new InputField($model, $attribute);
+    }
+}
